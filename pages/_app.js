fd447f40cd789618f75a40e0c34f5ba66f86acbb
Shivansh Vij <shivanshvij@outlook.com>
2022-08-03 18:16:34 -0400

    Adding analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,14 @@
 import "../styles/globals.css";
 import Head from "next/head";
+import { usePanelbear } from '@panelbear/panelbear-nextjs';
 
 export default function Frisbee({ Component, pageProps }) {
+ usePanelbear('HjrnquHievf', { scriptSrc: "/bear.js" });
   return (
     <>
       <Head>
         <title>Frisbee Documentation</title>
-        <meta property="og:title" content="Frisbee Documentation" key="title" />
+        <meta property="og:title" content="fRPC Documentation" key="title" />
       </Head>
       <Component {...pageProps} />
     </>
